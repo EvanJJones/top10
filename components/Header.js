5873999c2391f9b195f385b65e6fe7f7b823409b
Evan Jones <evanjones@fastmail.net>
2019-12-20 01:03:11 -0500

    material ui basics

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,49 @@
-import Link from 'next/link';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Link from "next/link";
 
-const linkStyle = {
-  marginRight: 15,
-};
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
-const Header = () => (
-  <div>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
-  </div>
-);
+export default function Header() {
+  const classes = useStyles();
 
-export default Header;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Top10
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Link href="/about">
+            <Button color="inherit">About</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,16 +1,26 @@
-import Header from './Header';
+import Header from "./Header";
+import Head from "next/head";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const layoutStyle = {
   margin: 20,
   padding: 20,
-  border: '1px solid #DDD',
+  border: "1px solid #DDD"
 };
 
-const Layout = (props) => (
-  <div style={layoutStyle}>
+const Layout = props => (
+  <>
+    <CssBaseline />
+    <Head>
+      <title>Top 10</title>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+      />
+    </Head>
     <Header />
     {props.children}
-  </div>
+  </>
 );
 
 export default Layout;
